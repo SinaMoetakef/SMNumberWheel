84612dd024f1a7c71957ec6c6149964fe700b5c2
Sina Moetakef <sina.moetakef@gmail.com>
2017-01-15 21:41:23 -0500

    merge

--- Example/Pods/Target Support Files/Pods-SMNumberWheel_Example/Pods-SMNumberWheel_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SMNumberWheel_Example : NSObject
-@end
-@implementation PodsDummy_Pods_SMNumberWheel_Example
-@end
--- Example/Pods/Target Support Files/Pods-SMNumberWheel_Tests/Pods-SMNumberWheel_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SMNumberWheel_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SMNumberWheel_TestsVersionString[];
-
